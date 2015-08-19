12d0e15af8bd32f875c7f991b410ef48be9d605f
Zhou Yuchao <513629287@qq.com>
2015-08-18 20:29:03 -0500

    add one more stupic solution for two sum

--- Two Sum.java
+++ Two Sum.java
@@ -1,3 +1,26 @@
+/** 
+ * solution 1
+ * O(n2) runtime, O(1) space
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums.length < 2)
+            return null;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+/**
+ * solution 2
+ * O(n) runtime, O(n) space
+ */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         if (nums.length <= 1)
@@ -15,3 +38,32 @@ public class Solution {
         throw new IllegalArgumentException("No two sum solution");
     }
 }
+
+/**
+ * solution 2
+ * O(n) runtime, O(n) space
+ **/
+ public class Solution {
+     // key:number, value:index
+     public int[] twoSum(int[] nums, int target) {
+         // error check
+         if (nums.length < 2) 
+            return null;
+            
+         HashMap<Integer, Integer> maps = new HashMap<Integer, Integer>();
+         for (int i = 0; i < nums.length; i++) {
+             // find it
+             // check key exists in hashmap, use map.containsKey(key)
+             if (maps.containsKey(target - nums[i])) {
+                 // be careful for the order of result, the num find in hashtable should in the first place
+                 // because we will always find target at the second num, the first num is already put into the hashtable
+                 // get value from hashmap, use map.get(key)
+                 return new int[] {maps.get(target - nums[i]), i + 1};
+             } 
+             // put key, value to map, use map.put(key, value)
+             maps.put(nums[i], i + 1);
+         }
+         throw new IllegalArgumentException("No two sum solution");
+     }
+ }
+
